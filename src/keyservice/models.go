04f8afdb18c919b686c8c8cf22423b70184f1a2d
darryl west <darryl.west@RainCitySoftware.com>
2015-12-19 19:42:52 -0800

    added private local key to test fixtures

---
+++ src/keyservice/models.go
@@ -0,0 +1,45 @@
+package keyservice
+
+import (
+    "time"
+)
+
+type DocumentIdentifier struct {
+    id string
+    dateCreated time.Time
+    lastUpdated time.Time
+    version int64
+}
+
+type User struct {
+    doi  DocumentIdentifier
+    username string
+    fullname string
+    email string
+    sms string
+    status string // Active, Inactive, Banned
+}
+
+type UserDocument struct {
+    doi DocumentIdentifier
+    owner string // User.doi.id
+    name string
+    meta string
+    share string // User.doi.id
+    expires time.Time
+    status string // Valid|Expired
+}
+
+type ChallengeCode struct {
+    doi DocumentIdentifier
+    challengeType string // Document, Access
+    sendTo string
+    sendDate time.Time
+    expires time.Time
+    status string // Active, Canceled, Expired
+}
+
+type AccessKey struct {
+    id string
+    key []byte
+}
