85962f8fc26a23830d102a8cd67825373ff0e1b6
darryl west <darryl.west@RainCitySoftware.com>
2015-12-19 20:24:33 -0800

    added model tests

---
+++ test/models_test.go
@@ -0,0 +1,19 @@
+package keyservicetest
+
+import (
+	//"keyservice"
+	"testing"
+
+	//"fmt"
+	//"github.com/darrylwest/cassava-logger/logger"
+
+	. "github.com/franela/goblin"
+)
+
+func TestModels(t *testing.T) {
+    g := Goblin(t)
+
+    g.Describe("Models", func() {
+        g.It("should do test all the document identifier methods...")
+    })
+}
