fecd92920a17de501d34611c7a704485dea501bd
darryl west <darryl.west@RainCitySoftware.com>
2015-12-22 19:49:18 -0800

    added bad decode test

--- test/models_test.go
+++ test/models_test.go
@@ -118,6 +118,14 @@ func TestModels(t *testing.T) {
 			// fmt.Printf("%v\n", msg)
 			g.Assert(err != nil).IsTrue()
 			g.Assert(msg == nil).IsTrue()
+
+			str = "e388X370815d453c3158178f549c94e3a5eb4efc1d4e6a450e9e0a2c998f801a:dc150713852bbbea8c4c202f59a6386471590c6aac557e4d784200369aeb9936038df223933b74654d3f1170cd6776a1e1899f257a109a25131d9844c19cd809:5:7e3df2eae53f770a0480214982828378e83e4e415e3e311d8fb0145d6cca275b:587b2d753c8409bbf876e7f9dc682b01a411cdd2ce6f0c66046d69c6343c1a1d:35ce0b60e9de37a4dcd7162e2c1fe365782992d0b7debe01c5731972cab8247a5f816a434759ccba4bcaf46860e4875e7f950db06a7657da727c0d928bf516123e13f25d958125e882f35dc4a9f7b40cc67b7237b1eaebeb03c43cd3b79bf4ffd2594ea4fd360bb7cf2ca180a9818f63af62c0883faea7ad96b46d194d"
+
+			msg, err = keyservice.DecodeMessageFromString( str )
+
+			// fmt.Printf("%v\n", msg)
+			g.Assert(err != nil).IsTrue()
+			g.Assert(msg == nil).IsTrue()
 		})
     })
 }
